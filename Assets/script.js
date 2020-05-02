8b14ffcbf8bd276f49f50d5c37448a80690e27a2
Eda Tuncer <edatezcanli@gmail.com>
2020-05-01 17:43:30 -0700

    added click events to buttons and added local storage for saving the text area.

---
+++ Assets/script.js
@@ -0,0 +1,123 @@
+// Printing the current date to the top of the page. 
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+console.log(currentDay);
+
+
+// Getting the current hour and format it.
+var currentHour = moment().hour();
+console.log(currentHour);
+
+// Adding onclick event on the saveBtn to capture action  
+/// Getting the value on the this element
+
+// Button
+var saveButtons = $('.saveBtn'),
+    tasks = JSON.parse(localStorage.getItem('tasks')) || {};
+console.log(tasks)
+
+
+for (var textareaid in tasks) {    /// loop an object (for (var key in obj){  console.log(key, obj[key])})
+    console.log(textareaid, tasks[textareaid])
+
+    $("#" + textareaid).val(tasks[textareaid])
+}
+
+ // getting the current hour and changing the colors by adding existing css classes.
+for (var i = 9; i < 18; i++) {
+    if (i < currentHour) {
+        $("#task-" + i).parent().addClass("past")
+    } else if (i === currentHour) {
+        $("#task-" + i).parent().addClass("present")
+    } else {
+        $("#task-" + i).parent().addClass("future")
+    }
+}
+
+saveButtons.on('click', function () {
+
+    //Referencing that specific button by calling 'this' inside of a shared call back function ,
+    // console.log(this);
+    // var myClickedButton=$(this)
+    // console.log('click', this);
+    // To associate the button with the hour:
+    var buttonHour = $(this).data('hour');
+    // the same data function works as a setter and you can add a second value to set the hour. this single one works to get the value. 
+    // console.log(myClickedButton.data('hour'));
+    // event.preventDefault();
+    var taskInput = ($('#task-' + buttonHour));
+    var taskValue = taskInput.val();
+    tasks['task-' + buttonHour] = taskValue;
+    console.log(tasks)
+    // localStorage.setItem ('task-' +buttonHour, taskInput.val() ); 
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+});
+
+// show the info on the DOM
+
+// var storedTasks = JSON.parse(localStorage.getItem('tasks') );
+// if (storedTasks ) {
+//     tasks = storedTasks;
+// }
+
+// $("textarea").each(function(){
+//     let i  = parseInt($(this).attr("id").split("-")[1])
+//     if (i < currentHour) {
+//         $(this).parent().addClass("past")
+//     } else if (i === currentHour) {
+//         $(this).parent().addClass("present")
+//     } else {
+//         $(this).parent().addClass("future")
+//     }
+
+// })
+
+// Storing as an object and put it into local storage and how to get that back and reassign them for storage
+
+// var myClickedButton=$(this);
+// // data-hour
+// console.log (data)
+//     // Read an attribute from the element to determine the hour it's supposed to represent
+//     var hour =$(this).attr("hour")
+// // Grab user input 
+// var text = $(this).siblings("textarea").val()
+// // saving this info in the localstorage  {9:txt,10:txt}  // loclstorage
+// $(".saveBtn").on("click", function (event){
+//     event.preventDefault();
+//     localStorage.setItem("text", JSON.stringify(textarea));
+// })
+
+
+// // Get the current hour of the day and compare with the hour blocks on the page
+// /// * Determine if each hour block is past, present or future
+// $('.saveBtn').on('click', function(){
+//         let saveID = $(this).attr('hour');
+//         let saveText = $(this).parent().siblings().children('.description').val();
+//         localStorage.setItem(saveID, saveText);
+//     });
+// });
+
+// $('.saveBtn').on('click', function(){
+//     value = $(this).siblings("textarea").val();
+//     hourString = $(this).siblings("div").text();
+//     saveSchedule(hourString, value);
+//   });
+
+
+// // Select all the elements with the "hour-block" class and loop over each one.
+// $('.hour-block').each(function() {
+//     hourBlock.addClass('present');
+//     var hourBlock = $(this);
+
+// }
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Scheduler
-Workday Scheduler to organize your days
+A simple calendar application that allows the user to save events for each hour of the workday day from 9AM to 5PM. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
+
+This application uses jquery and moment.js, and Bootstrap.
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+
+### Walkthrough the Scheduler:
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+
+The following animation demonstrates the application functionality:
+
+![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
